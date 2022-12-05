3e64b9f87396e02c7aa5524fd33ab82b95819d0d
KN Bhavya <bhavya.kn@datagrokr.com>
2022-12-05 14:00:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import unittest
+class calculator:
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return x + y
+
+    def sub(self, x, y):
+        return x-y
+
+    def mul(self, x, y):
+        return x*y
+
+class testCalci(unittest.TestCase):
+    def test_add(self):
+        self.calc = calculator()
+        result=self.calc.add(4, 4)
+        expected=8
+        self.assertEqual(result, expected)
+
+    def test_sub(self):
+        self.calc = calculator()
+        result = self.calc.sub(8, 4)
+        expected=4
+        self.assertEqual(result, expected)
+
+    def test_mul(self):
+        self.calc = calculator()
+        result = self.calc.mul(4, 4)
+        expected=16
+        self.assertEqual(result, expected)
+
+unittest.main(argv = [''],verbosity=2, exit=False)
+
+
+
+
+
